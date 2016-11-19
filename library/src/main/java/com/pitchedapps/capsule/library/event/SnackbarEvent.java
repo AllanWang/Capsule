c5cd3fdb6360322177fc4da05a28fb56ab8693f1
AllanWang <me@allanwang.ca>
2016-11-19 18:08:37 -0500

    snackbar bg color

--- library/src/main/java/com/pitchedapps/capsule/library/event/SnackbarEvent.java
+++ library/src/main/java/com/pitchedapps/capsule/library/event/SnackbarEvent.java
@@ -1,10 +1,13 @@
 package com.pitchedapps.capsule.library.event;
 
 import android.content.Context;
+import android.support.annotation.ColorInt;
+import android.support.annotation.ColorRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,7 +21,7 @@ import com.pitchedapps.capsule.library.utils.ResUtils;
 public class SnackbarEvent {
 
     private String text, actionText;
-    private int textId = -1, actionTextId = -1;
+    private int textId = -1, actionTextId = -1, colorInt = -1, colorRes = -1;
     private int duration = Snackbar.LENGTH_LONG;
     private View.OnClickListener mListener;
 
@@ -47,16 +50,33 @@ public class SnackbarEvent {
         return this;
     }
 
+    public SnackbarEvent setColorRes(@ColorRes int color) {
+        colorRes = color;
+        return this;
+    }
+
+    public SnackbarEvent setColor(@ColorInt int color) {
+        colorInt = color;
+        return this;
+    }
+
     //It is guaranteed that one of the two contains a value
     private String getText(Context context, String s, @StringRes int si) {
         if (si != -1) return ResUtils.s(context, si);
         return s;
     }
 
+    private int getColor(Context context, @ColorRes int colorRes, @ColorInt int colorInt) {
+        if (colorRes != -1) return ContextCompat.getColor(context, colorRes);
+        return colorInt;
+    }
+
     public void load(@NonNull View view) {
         Snackbar snackbar = Snackbar.make(view, getText(view.getContext(), text, textId), duration);
         if (mListener != null)
             snackbar.setAction(getText(view.getContext(), actionText, actionTextId), mListener);
+        int bgColor = getColor(view.getContext(), colorRes, colorInt);
+        if (bgColor != -1) snackbar.getView().setBackgroundColor(bgColor);
         snackbar.show();
     }
 }
