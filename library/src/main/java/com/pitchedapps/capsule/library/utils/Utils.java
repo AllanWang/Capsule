aff76b6f361b8d71fc19968190dcd01fb80b374d
AllanWang <me@allanwang.ca>
2016-12-21 03:21:08 -0500

    emailbuilder

--- library/src/main/java/com/pitchedapps/capsule/library/utils/Utils.java
+++ library/src/main/java/com/pitchedapps/capsule/library/utils/Utils.java
@@ -1,5 +1,9 @@
 package com.pitchedapps.capsule.library.utils;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Build;
 
 /**
@@ -14,5 +18,36 @@ public class Utils {
 
     public static boolean isLollipopPlus() {
         return Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP;
+    }
+
+    public static String getAppVersion(Context context) {
+        try {
+            return context.getPackageManager().getPackageInfo(context.getPackageName(), 0).versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            // this should never happen
+            return "Unknown";
+        }
+    }
+
+    public static String getAppPackageName(Context context) {
+        return context.getPackageName();
+    }
+
+    public static boolean hasNetwork(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+    }
+
+    public static boolean isAppInstalled(Context context, String packageName) {
+        final PackageManager pm = context.getPackageManager();
+        boolean installed;
+        try {
+            pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+            installed = true;
+        } catch (PackageManager.NameNotFoundException e) {
+            installed = false;
+        }
+        return installed;
     }
 }
