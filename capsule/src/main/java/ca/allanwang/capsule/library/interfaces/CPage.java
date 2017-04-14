90f7beb808865833d865cf070aa91a6cbdaba75c
Allan Wang <me@allanwang.ca>
2017-04-14 01:38:59 -0400

    cpage

--- capsule/src/main/java/ca/allanwang/capsule/library/interfaces/CPage.java
+++ capsule/src/main/java/ca/allanwang/capsule/library/interfaces/CPage.java
@@ -1,13 +1,11 @@
 package ca.allanwang.capsule.library.interfaces;
 
-import ca.allanwang.capsule.library.fragments.CapsulePageFragment;
-
 /**
  * Created by Allan Wang on 2016-10-30.
  */
 
 public interface CPage extends CFragmentCore {
 
-    CapsulePageFragment getFragment();
+    CPageFragment getFragment();
 
 }
