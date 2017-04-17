c237b6fc1844275489de7ca0f5ddae8204afa9ec
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2017-04-17 12:00:26 -0500

    Update libraries and add CollapseListener

---
+++ capsule/src/main/java/ca/allanwang/capsule/library/interfaces/CCollapseListener.java
@@ -0,0 +1,39 @@
+package ca.allanwang.capsule.library.interfaces;
+
+import android.support.design.widget.AppBarLayout;
+
+public abstract class CCollapseListener implements AppBarLayout.OnOffsetChangedListener {
+
+    public enum State {
+        EXPANDED,
+        COLLAPSED,
+        IDLE
+    }
+
+    private State mCurrentState = State.IDLE;
+
+    @Override
+    public final void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+        onVerticalOffsetChanged(appBarLayout, verticalOffset);
+        if (verticalOffset == 0) {
+            if (mCurrentState != State.EXPANDED) {
+                onStateChanged(appBarLayout, State.EXPANDED);
+            }
+            mCurrentState = State.EXPANDED;
+        } else if (Math.abs(verticalOffset) >= appBarLayout.getTotalScrollRange()) {
+            if (mCurrentState != State.COLLAPSED) {
+                onStateChanged(appBarLayout, State.COLLAPSED);
+            }
+            mCurrentState = State.COLLAPSED;
+        } else {
+            if (mCurrentState != State.IDLE) {
+                onStateChanged(appBarLayout, State.IDLE);
+            }
+            mCurrentState = State.IDLE;
+        }
+    }
+
+    public abstract void onStateChanged(AppBarLayout appBarLayout, State state);
+
+    public abstract void onVerticalOffsetChanged(AppBarLayout appBarLayout, int verticalOffset);
+}
